cc798190682e9687f9b8c90711be27f33a05048e
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-01-31 01:08:54 +0100

    url

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const url = window.location.href;
 
-fetch(`${url}static/data.json`)
+fetch(`${url}data.json`)
   .then(x => x.json())
   .then(y => {
     document.getElementById('start-btn').classList.remove('btn-hide');

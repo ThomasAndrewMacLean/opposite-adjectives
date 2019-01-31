1bf20c6e82df2bfe2ab7b76ad5c5a49224a15a33
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-01-31 02:05:35 +0100

    buttons and stuff 🎉

--- src/script.js
+++ src/script.js
@@ -10,16 +10,42 @@ if (url === 'http://localhost:1234/') {
   url += 'static/';
 }
 
-fetch(`${url}data.json`)
-  .then(x => x.json())
-  .then(y => {
-    document.getElementById('start-btn').classList.remove('btn-hide');
-    console.log(y);
-  });
-
-document.getElementById('start-btn').addEventListener('click', () => {
+const pageTransition = time => {
   document.getElementById('transition').classList.remove('transition-hide');
   setTimeout(() => {
     document.getElementById('transition').classList.add('transition-hide');
-  }, 200);
+  }, time);
+};
+
+let data;
+let counter = 0;
+
+const setWords = (counter, ans) => {
+  document.getElementById('word').innerText = data[counter][ans];
+};
+
+document.getElementById('page2').addEventListener('touchstart', () => {
+  setWords(counter, 2);
+});
+document.getElementById('page2').addEventListener('touchend', () => {
+  setWords(counter, 1);
+});
+document.getElementById('next-btn').addEventListener('click', event => {
+  event.stopPropagation();
+  counter++;
+  setWords(counter, 1);
+});
+
+fetch(`${url}data.json`)
+  .then(x => x.json())
+  .then(d => {
+    document.getElementById('start-btn').classList.remove('btn-hide');
+    console.log(d);
+    data = d;
+  });
+
+document.getElementById('start-btn').addEventListener('click', () => {
+  pageTransition(200);
+  document.getElementById('page1').style.height = 0;
+  setWords(counter, 1);
 });
